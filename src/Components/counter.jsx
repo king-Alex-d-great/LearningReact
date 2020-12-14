8f0c1a18ce49a3df65038164c4ae50e7541d3e8c
ogubuike alex <ogubuikealex@gmail.com>
2020-12-14 15:34:49 +0100

    learning to Embed Expressions

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -4,6 +4,8 @@ class Counter extends Component {
     //state = {  }
     render() { 
         return (
+            //we must have one parent element to contain our react application
+        //instead of the div, we could use React.fragment, this prevents having an unnecessary div
         <div>
             <h1>hey buddu</h1>
             <button>increment</button> 
